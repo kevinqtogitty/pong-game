2a3f9c124f21783e8a5484b7dc7cb86f7d716819
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-02-19 17:49:52 -0500

    Update: cleaned up code and comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,69 +1,49 @@
-import React, { useRef, useState } from 'react';
-import { Environment, Html, OrbitControls, Stars } from '@react-three/drei';
-import { Canvas, useFrame, useThree } from '@react-three/fiber';
-import { Mesh } from 'three';
-import { useControls, folder } from 'leva';
-import { Paddle } from './components/Paddle';
+import React, { useEffect, useRef, useState } from 'react';
 import {
-  Debug,
-  Physics,
-  useBox,
-  usePlane,
-  useSphere
-} from '@react-three/cannon';
+  Center,
+  Environment,
+  Float,
+  Html,
+  Lightformer,
+  OrbitControls,
+  Stars,
+  Text3D,
+  useTexture
+} from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import { Paddle } from './components/Paddle';
+import { Debug, Physics } from '@react-three/cannon';
 import { PingPongBall } from './components/PingPongBall';
+import { useMainStore } from './state/mainStore';
+import Score from './components/Score';
+import Restart from './components/Restart';
 
 function App() {
   const ThreeScene = () => {
-    const [score, setScore] = useState(0);
+    const { setScore } = useMainStore();
     const handleScoreChange = (calculationKey: string) => {
-      calculationKey === '+' ? setScore((state) => state + 1) : setScore(0);
+      setScore(calculationKey);
     };
+
     return (
       <Canvas camera={{ position: [0, 20, 30], fov: 70 }}>
-        {/**
-         * Cameras
-         */}
-
-        {/**
-         * Lights
-         */}
+        {/* Lights */}
         <ambientLight />
-        <directionalLight position={[3, 3, 1]} color="#f3e99b" intensity={2} />
-
-        {/**
-         * Controls
-         */}
-        {/* <OrbitControls /> */}
-
-        {/**
-         * Helpers
-         */}
-
-        {/**
-         * Environment
-         */}
-        {/* <Stars /> */}
-        <Environment background preset="dawn" blur={0.8} />
-
-        {/**
-         * Objects
-         */}
+        <directionalLight position={[-3, 3, 1]} color="#f3e99b" intensity={2} />
+        <hemisphereLight />
+        {/* Environment */}
+        <Stars />
+        {/* Text */}
+        <Score />
+        <Restart />
+        {/* Objects */}
         <Physics
           gravity={[0, -80, 0]}
           defaultContactMaterial={{ restitution: 1.07 }}
         >
-          {/* <Debug color="red" scale={1}> */}
           <PingPongBall handleScoreChange={handleScoreChange} />
-          {/* <Box /> */}
-
           <Paddle handleScoreChange={handleScoreChange} />
-          {/* <BallCatchPlane /> */}
-          {/* </Debug> */}
         </Physics>
-        <Html fullscreen className="three-html">
-          Score: {score}
-        </Html>
       </Canvas>
     );
   };
