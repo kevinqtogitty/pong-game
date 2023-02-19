86b88eb8960b707c0237842c352baa73a4290349
kevinqtogitty <103936802+kevinqtogitty@users.noreply.github.com>
2023-02-19 17:47:11 -0500

    Update: added score and gamestate

--- src/components/PingPongBall.tsx
+++ src/components/PingPongBall.tsx
@@ -1,6 +1,7 @@
 import { usePlane, useSphere } from '@react-three/cannon';
-import { useThree } from '@react-three/fiber';
+import { useFrame, useThree } from '@react-three/fiber';
 import React, { useEffect, useState } from 'react';
+import { useMainStore } from '../state/mainStore';
 import { PaddleProps } from './Paddle';
 interface PingPongBallProps extends PaddleProps {}
 
@@ -8,23 +9,29 @@ export const PingPongBall: React.FC<PingPongBallProps> = ({
   args = [1.5, 512, 512],
   handleScoreChange
 }) => {
-  const [isResettable, setIsResettable] = useState(false);
+  const {
+    setTries,
+    setIsResettable,
+    isResettable,
+    setIsGameOver,
+    setHasStarted
+  } = useMainStore();
+
   const { viewport } = useThree();
+
   useEffect(() => {
     document.addEventListener('keydown', resetPosition);
     return () => document.removeEventListener('keydown', resetPosition);
   });
 
-  const [ref, api] = useSphere(() => ({
-    args,
-    mass: 5,
-    position: [0, 10, 0]
-  }));
-
   const resetPosition = (e: KeyboardEvent) => {
     if (isResettable && e.code === 'Space') {
       api.position.set(0, 10, 0);
-      setIsResettable(false);
+      handleScoreChange('-');
+      setIsResettable();
+      setTries();
+      setIsGameOver(false);
+      setHasStarted();
     } else {
       return;
     }
@@ -32,16 +39,22 @@ export const PingPongBall: React.FC<PingPongBallProps> = ({
 
   usePlane(() => ({
     rotation: [-Math.PI / 2, 0, 0],
-    position: [0, -viewport.height, 0],
+    position: [0, -viewport.height - viewport.height / 2, 0],
     onCollide: () => {
-      handleScoreChange('-');
       api.velocity.set(0, 0, 0);
-      setIsResettable(true);
+      setIsResettable();
+      setIsGameOver(true);
     }
   }));
 
+  const [ref, api] = useSphere(() => ({
+    args: [1.5],
+    mass: 50,
+    position: [0, -viewport.height - viewport.height / 2, 0]
+  }));
+
   return (
-    <mesh ref={ref} scale={1}>
+    <mesh scale={1} receiveShadow castShadow ref={ref}>
       <sphereBufferGeometry args={[1.5, 512, 512]} />
       <meshStandardMaterial color={'#fff'} />
     </mesh>
